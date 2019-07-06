abe761402a3117155b0152187ce5a09d71a7fc55
enas <enasm2477@gmail.com>
2019-07-06 17:55:05 +0200

    folders

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+/* global $ */
+
+$(function () {
+    'use strict';
+    $('.info-list li').click(function () {
+        $(this).addClass('selected').siblings('li').removeClass('selected');
+        $('.info-content div').hide();
+        $('.' + $(this).data('class')).fadeIn();
+    });
+});
+
+
+
---
+++ js/style.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	// menu click event
+	$('.menu-toggle').click(function() {
+		$('nav .line li').toggle('slow');
+        
+	});
+});
